17b67613ecafb1344e29c3705f31ca216f7b8255
Lanting Xie <77956911+shellyxielt@users.noreply.github.com>
2021-04-13 17:48:11 +0800

    Update show.js

--- static/js/show.js
+++ static/js/show.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.05,-114.06], 11);
+var mymap = L.map('mapid').setView([51.05,-114.06], 10);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
